cdadd177b05b4771075767437ea3a6739404013c
Moulaali <moulaalihujare@gmail.com>
2022-04-25 19:27:05 +0530

    done

---
+++ Asignment Project/project-5-springboot/src/main/java/com/example/project5/springboot/Project5SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.example.project5.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project5SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project5SpringbootApplication.class, args);
+		
+		System.out.println("Spring Boot Web-App Consloe is working ..");
+	}
+
+}
---
+++ Asignment Project/project-5-springboot/src/test/java/com/example/project5/springboot/Project5SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project5.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project5SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
